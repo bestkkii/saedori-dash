872809299eab241abca557bf0d7f3e4ba70b514d
rabbit0216 <rabbit1999k@gmail.com>
2025-04-25 22:17:21 +0900

    refactor: modify realtime search detail variable

--- components/detail/realtime_search_detail.py
+++ components/detail/realtime_search_detail.py
@@ -2,9 +2,9 @@ import dash_mantine_components as dmc
 from fetch.fetch_realtime_search_keyword_data import parse_realtime_search_keyword_data
 from dash import html
 
-def create_realtime_search_chart(country, keyword):
-        left_item = keyword[:5]
-        right_item = keyword[5:]
+def create_realtime_search_chart(country, realtime_search_keyword):
+        left_item = realtime_search_keyword[:5]
+        right_item = realtime_search_keyword[5:]
         return dmc.GridCol([
             dmc.Grid([
                 dmc.GridCol(dmc.Stack([
@@ -18,12 +18,12 @@ def create_realtime_search_chart(country, keyword):
             ])
         ], span=6)
 
-def create_realtime_search_chart_row(rank, keyword):
+def create_realtime_search_chart_row(rank, realtime_search_keyword):
     return dmc.Box([
         dmc.Grid([
             dmc.GridCol(dmc.Text(str(rank), fw=600), span=4),
-            dmc.GridCol(dmc.Text(keyword, fw=600), span="content"),
-            dmc.GridCol(html.Hr()),
+            dmc.GridCol(dmc.Text(realtime_search_keyword, fw=600), span="content"),
+            dmc.GridCol(dmc.Divider()),
                 
         ])
     ])
