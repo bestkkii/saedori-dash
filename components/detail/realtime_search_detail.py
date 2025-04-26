858afbd0539e0342c693bbcc41e5c4d86f8cc494
kakyung.lee <kakyung.lee@lge.com>
2025-04-26 15:33:42 +0900

    feat: modify design for realtime search detail

--- components/detail/realtime_search_detail.py
+++ components/detail/realtime_search_detail.py
@@ -8,22 +8,22 @@ def create_realtime_search_chart(country, realtime_search_keyword):
         return dmc.GridCol([
             dmc.Grid([
                 dmc.GridCol(dmc.Stack([
-                    create_realtime_search_chart_row(item["index"], item["value"])
-                    for item in left_item]), span=5
+                    create_realtime_search_chart_row(country, item["index"], item["value"])
+                    for item in left_item]), span=6
                 ),
                 dmc.GridCol(dmc.Stack([
-                    create_realtime_search_chart_row(item["index"], item["value"])
-                    for item in right_item]), span=5
-                )
-            ])
+                    create_realtime_search_chart_row(country, item["index"], item["value"])
+                    for item in right_item]), span=6
+                ),
+            ]),
         ], span=6)
 
-def create_realtime_search_chart_row(rank, realtime_search_keyword):
-    return dmc.Box([
-        dmc.Grid([
-            dmc.GridCol(dmc.Text(str(rank), fw=600), span=4),
-            dmc.GridCol(dmc.Text(realtime_search_keyword, fw=600), span="content"),
+def create_realtime_search_chart_row(country, rank, realtime_search_keyword):
+    rank_color = "red"
+    if country == "us":
+        rank_color = "blue"
+    return dmc.Grid([
+            dmc.GridCol(dmc.Avatar(str(rank), radius="sm", fw=500, color=rank_color), span=4,),
+            dmc.GridCol(dmc.Text(realtime_search_keyword, fw=500, fz="h5", ta="center"), span="content", offset=1),
             dmc.GridCol(dmc.Divider()),
-                
         ])
-    ])
